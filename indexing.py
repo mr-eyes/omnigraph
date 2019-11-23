cdf653be8d9669c6ee3716dfab4f0d96d4ab1f0a
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-11-24 00:49:29 +0200

    indexing with k21 q29

--- indexing.py
+++ indexing.py
@@ -4,9 +4,11 @@ import sys
 fasta_file = sys.argv[1]
 names_file = sys.argv[2]
 
-kf = kp.kDataFrameMQF(25)
+kf = kp.kDataFrameMQF(21, 29, 1)
 
 # Mode 1 activates the kmers mode
-ckf = kp.index(kf, {"mode" : 1}, fasta_file , 1000, names_file)
+ckf = kp.index(kf, {"mode" : 1}, fasta_file , 10000, names_file)
+
+print("Serializing the index ...")
 
 ckf.save("index/idx_gencode.v32.transcripts")
