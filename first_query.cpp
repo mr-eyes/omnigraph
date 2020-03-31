ae03bf034f849c43e269fb7eea8118ab5ee3cdf0
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-03-31 21:19:03 +0200

    parse sqlite_db from config.ini

--- first_query.cpp
+++ first_query.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main(int argc, char **argv) {
 
     string config_file_path = "../config.ini";
-    string index_prefix, PE_1_reads_file, PE_2_reads_file;
+    string index_prefix, PE_1_reads_file, PE_2_reads_file, sqlite_db;
     int batchSize;
 
     INIReader reader(config_file_path);
@@ -26,12 +26,13 @@ int main(int argc, char **argv) {
     index_prefix = reader.Get("kProcessor", "idx_prefix", "");
     PE_1_reads_file = reader.Get("Reads", "read1", "");
     PE_2_reads_file = reader.Get("Reads", "read2", "");
+    sqlite_db = reader.Get("SQLite", "db_file", "query1_result.db");
     batchSize = reader.GetInteger("kProcessor", "chunk_size", 1);
 
     cerr << "Processing: \nR1: " << PE_1_reads_file << "\nR2: " << PE_2_reads_file << endl;
 
 
-    firstQuery *Query = new firstQuery(PE_1_reads_file, PE_2_reads_file, index_prefix, batchSize);
+    firstQuery *Query = new firstQuery(PE_1_reads_file, PE_2_reads_file, index_prefix, sqlite_db, batchSize);
     Query->start_query();
 
     for (int p = 1; p <= 2; p++) {
