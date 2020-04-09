5c6f461b323cf02acbe00e0aadc0f8121871b2e8
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-04-09 19:26:29 +0200

    slurm secondQuery job

--- job_secondQuery.sh
+++ job_secondQuery.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mabuelanin@gmail.com
 #SBATCH -p bmh
-#SBATCH -J firstQuery
+#SBATCH -J secondQuery
 #SBATCH --time=15:00:00
 #SBATCH -N 1
 #SBATCH -n 1
@@ -20,24 +20,31 @@ set -x
 
 # make a directory specific to user and job
 export MYTMP=/scratch/${USER}/slurm_${SLURM_JOB_NAME}_${SLURM_JOBID}
-mkdir -p $MYTMP
+mkdir -p "$MYTMP"
 
 # force clean it up
 function cleanup() { 
-    rm -rf $MYTMP; 
+    rm -rf "$MYTMP";
 }
 
 trap cleanup EXIT
 
 # <run your code, telling it to use that dir;
 
-cd $MYTMP
-touch ${MYTMP}/scratch_fullData_firstQuery.log
-echo "running in $(pwd)" > ${MYTMP}/scratch_fullData_firstQuery.log
+cd "$MYTMP"
 
-/usr/bin/time -v /home/mhussien/omnigraph/build/query_1 --db ${MYTMP}/query_1.db #&> ${MYTMP}/scratch_fullData_firstQuery.log
+DB_FILE=/home/mhussien/omnigraph/slurm_firstQuery_20290252/query_1.db
 
-cp -r $MYTMP $SLURM_SUBMIT_DIR
+echo "running in $(pwd)"
+
+# Copy database file, to be removed later
+cp $DB_FILE "$MYTMP"/
+
+/usr/bin/time -v /home/mhussien/omnigraph/build/query_2 --db "${MYTMP}"/query_1.db --out-dir "${MYTMP}"/fasta_out
+
+rm "$MYTMP"/$DB_FILE
+
+cp -r "$MYTMP" "$SLURM_SUBMIT_DIR"
 
 scontrol show job $SLURM_JOB_ID     # Print out final statistics about resource uses before job exits
 
