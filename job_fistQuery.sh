6d547dabcd21dd77b45c1f30baaefcd3b32cbc44
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-04-09 02:27:59 +0200

    finally worked after 43 trials

--- job_fistQuery.sh
+++ job_fistQuery.sh
@@ -2,43 +2,48 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mabuelanin@gmail.com
 #SBATCH -p bmh
-#SBATCH -J sgc
+#SBATCH -J firstQuery
 #SBATCH --time=15:00:00
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -c 1
-#SBATCH --mem=60gb
+#SBATCH --mem=16gb
+#SBATCH --output=slurm_%x.%j.out
+#SBATCH --error=slurm_%x.%j.out
 
 # exit when any command fails
 set -e
+set -o nounset
+set -o errexit
+set -x
 
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-
-# activate conda in general
-. "/home/mhussien/miniconda3/etc/profile.d/conda.sh"
-
-conda activate omnigraph
-
-cd /home/mhussien/omnigraph/build
 
 # make a directory specific to user and job
-export MYTMP=/scratch/${USER}/slurm_${SLURM_JOBID}
-mkdir -p ${MYTMP}
+export MYTMP=/scratch/${USER}/slurm_${SLURM_JOB_NAME}_${SLURM_JOBID}
+mkdir -p $MYTMP
 
 # force clean it up
-function cleanup() { rm -rf $MYTMP; }
+function cleanup() { 
+    rm -rf $MYTMP; 
+}
+
 trap cleanup EXIT
 
 # <run your code, telling it to use that dir;
-echo running in $(pwd)
 
-/usr/bin/time -v ./query_1 ${MYTMP}/scratch_query1_result.db &> /home/mhussien/omnigraph/logs/fullData_firstQuery.log
-cp ${MYTMP}/scratch_query1_result.db ${SLURM_SUBMIT_DIR}
+cd $MYTMP
+touch ${MYTMP}/scratch_fullData_firstQuery.log
+echo "running in $(pwd)" > ${MYTMP}/scratch_fullData_firstQuery.log
 
-scontrol show job ${SLURM_JOB_ID}     # Print out final statistics about resource uses before job exits
+/usr/bin/time -v /home/mhussien/omnigraph/build/query_1 --db ${MYTMP}/query_1.db #&> ${MYTMP}/scratch_fullData_firstQuery.log
+
+cp -r $MYTMP $SLURM_SUBMIT_DIR
+
+scontrol show job $SLURM_JOB_ID     # Print out final statistics about resource uses before job exits
 
 # Print out values of the current jobs SLURM environment variables
 env | grep SLURM
+
+scontrol show job ${SLURM_JOB_ID}     # Print out final statistics about resource uses before job exits
+
+sstat --format 'JobID,MaxRSS,AveCPU' -P ${SLURM_JOB_ID}.batch
