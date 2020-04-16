87466533294755c9cfe6f52a802960d346cedcae
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-04-16 07:30:30 +0200

    fix some bugs

--- scripts/unitigs_to_viz_kmersHistograms.py
+++ scripts/unitigs_to_viz_kmersHistograms.py
@@ -33,7 +33,7 @@ KM: print
 
 class Stats:
     km_stats = dict()
-    km_max = dict()
+    ln_max = dict()
     ln_stats = dict()
     ln_prints = {"==75": "Frequency of unitigs with length == 75",
                  "76:80": "Sum of frequencies of unitigs length [76:80]",
@@ -49,65 +49,78 @@ class Stats:
                  ">10000": "Frequency of unitigs with KM [> 10000.0]"}
 
     def __init__(self, number_of_components):
-        self.km_max = 0
-        self.km_keys = ["==75", "76:80", "81:151", ">151"]
-        self.ln_keys = ["1:2", "2:10", "10:50", "50:100", "100:1000", "1000:10000", ">10000"]
+        self.ln_keys = ["==75", "76:80", "81:151", ">151"]
+        self.km_keys = ["1:2", "2:10", "10:50", "50:100", "100:1000", "1000:10000", ">10000"]
         for _compID in range(1, number_of_components + 1, 1):
             self.number_of_components = number_of_components
             self.km_stats[_compID] = dict()
             self.ln_stats[_compID] = dict()
+            self.ln_max[_compID] = 0
             for k in self.km_keys:
-                self.km_stats[_compID][key] = 0
+                self.km_stats[_compID][k] = 0
             for k in self.ln_keys:
-                self.ln_stats[_compID][key] = 0
-
-    def stats_insert_ln(self, _compID, ln):
-        if ln > 10000:
-            self.ln_stats[_compID][">10000"] += 1
-        elif 1000 < ln <= 10000:
-            self.ln_stats[_compID]["1000:10000"] += 1
-        elif 50 > ln <= 100:
-            self.ln_stats[_compID]["50:100"] += 1
-        elif 10 > ln <= 50:
-            self.ln_stats[_compID]["10:50"] += 1
-        elif 2 > ln <= 10:
-            self.ln_stats[_compID]["2:10"] += 1
-        elif 1 > ln <= 2:
-            self.ln_stats[_compID]["1:2"] += 1
+                self.ln_stats[_compID][k] = 0
 
     def stats_insert_km(self, _compID, km):
-        if km > self.km_max:
-            self.km_max = km
-        if km > 151:
-            self.km_stats[_compID][">151"] += 1
-        elif 81 <= km <= 151:
-            self.km_stats[_compID]["81:151"] += 1
-        elif 76 <= km <= 80:
-            self.km_stats[_compID]["76:80"] += 1
-        elif km == 75:
-            self.km_stats[_compID]["==75"] += 1
+        #        print(f"Inserting {ln} in comp: {_compID}")
+        #        print(f"type: {type(_compID)}")
+
+        if km > self.ln_max[compID]:
+            self.ln_max[_compID] = km
+
+        if km > 10000:
+            self.km_stats[_compID][">10000"] += 1
+        elif 1000 < km <= 10000:
+            self.km_stats[_compID]["1000:10000"] += 1
+        elif 100 < km <= 1000:
+            self.km_stats[_compID]["100:1000"] += 1
+        elif 50 > km <= 100:
+            self.km_stats[_compID]["50:100"] += 1
+        elif 10 > km <= 50:
+            self.km_stats[_compID]["10:50"] += 1
+        elif 2 > km <= 10:
+            self.km_stats[_compID]["2:10"] += 1
+        elif 0 > km <= 2:
+            self.km_stats[_compID]["1:2"] += 1
+
+    #        print(f"Done : {str(self.ln_stats[_compID])}")
+
+    def stats_insert_ln(self, _compID, ln):
+
+        if ln > 151:
+            self.ln_stats[_compID][">151"] += 1
+        elif 81 <= ln <= 151:
+            self.ln_stats[_compID]["81:151"] += 1
+        elif 76 <= ln <= 80:
+            self.ln_stats[_compID]["76:80"] += 1
+        elif ln == 75:
+            self.ln_stats[_compID]["==75"] += 1
 
     def get_stats_of_comp(self, _compID):
         print(f"Stats for component {_compID}:\n")
-        print(f"LN Field Stats: ")
+        print(f"\tLN Field Stats: ")
+        print(f"\t\t - Max Ln:  {self.ln_max[_compID]} ")
         for k, v in self.ln_stats[_compID].items():
-            print(f"\t - {self.ln_prints[k]} : {v}")
+            print(f"\t\t - {self.ln_prints[k]} : {v}")
 
-        print(f"\nKM Field Stats: ")
+        print(f"\n\tKM Field Stats: ")
         for k, v in self.km_stats[_compID].items():
-            print(f"\t - {self.km_prints[k]} : {v}")
+            print(f"\t\t - {self.km_prints[k]} : {v}")
 
     def get_stats_all_comps(self):
         print(f"Stats for all components: \n")
 
         tmp_km_stats = dict()
         tmp_ln_stats = dict()
-        for _compID in range(1, self.number_of_components + 1, 1):
-            for k in self.km_keys:
-                tmp_km_stats[key] = 0
-            for k in self.ln_keys:
-                tmp_ln_stats[key] = 0
+        max_ln = max(list(self.ln_max.values()))
 
+
+        for k in self.km_keys:
+            tmp_km_stats[k] = 0
+        for k in self.ln_keys:
+            tmp_ln_stats[k] = 0
+
+        for _compID in range(1, self.number_of_components + 1, 1):
             for k, v in self.ln_stats[_compID].items():
                 tmp_ln_stats[k] += v
 
@@ -115,11 +128,12 @@ class Stats:
                 tmp_km_stats[k] += v
 
         print(f"LN Field Stats: ")
+        print(f"\n - Max LN: {max_ln}")
         for k, v in tmp_ln_stats.items():
             print(f"\t - {self.ln_prints[k]} : {v}")
 
         print(f"\nKM Field Stats: ")
-        for k, v in tmp_ln_stats.items():
+        for k, v in tmp_km_stats.items():
             print(f"\t - {self.km_prints[k]} : {v}")
 
 
@@ -155,7 +169,7 @@ component_to_unitigs = dict()
 with open(components_csv, 'r') as compsReader:
     for line in compsReader:
         line = list(map(int, line.strip().split(',')))
-        compID = line[0]
+        compID = int(line[0])
         component_to_unitigs[compID] = list()
         KM[compID] = list()
         KC[compID] = list()
@@ -183,6 +197,11 @@ with open(unitigs_file, 'r') as unitigsReader:
         KM[compID].append(_km)
         stats.stats_insert_km(compID, _km)
 
+
+stats.get_stats_all_comps()
+print("\n ------------------------------ \n")
+stats.get_stats_of_comp(focus_compID)
+
 print("Generating HTML viz...")
 
 figs = {
@@ -191,9 +210,6 @@ figs = {
     "LN": go.Figure(data=[go.Histogram(x=LN[focus_compID])]),
 }
 
-print("\n ------------------------------ \n ")
-stats.get_stats_all_comps()
-stats.get_stats_of_comp(focus_compID)
 
 for key, fig in figs.items():
     # print(f"Saving {key} HTML")
