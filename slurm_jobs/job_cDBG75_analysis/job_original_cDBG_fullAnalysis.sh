1816bd18fb0a950c65e823f7046d2d08701cefee
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-04-19 01:07:46 +0200

    original @ 0.985 job

---
+++ slurm_jobs/job_cDBG75_analysis/job_original_cDBG_fullAnalysis.sh
@@ -0,0 +1,120 @@
+#! /bin/bash -login
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mabuelanin@gmail.com
+#SBATCH -p bmh
+#SBATCH -J cDBG75_0.985_original
+#SBATCH --time=4:00:00
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 32
+#SBATCH --mem=50gb
+#SBATCH --output=slurm_%x.%j.out
+#SBATCH --error=slurm_%x.%j.err
+
+
+# activate conda in general (MUST BE PLACE BEFORE ANYTHING)
+. /home/mhussien/miniconda3/etc/profile.d/conda.sh
+conda activate omnigraph
+
+
+# exit when any command fails
+set -e
+set -o nounset
+set -o errexit
+set -x
+set -euox pipefail
+
+
+# make a directory specific to user and job
+export MYTMP=/scratch/${USER}/slurm_${SLURM_JOB_NAME}_${SLURM_JOBID}
+mkdir -p "$MYTMP"
+
+# force clean it up
+function cleanup() {
+    rm -rf "$MYTMP";
+}
+
+trap cleanup EXIT
+
+# ~~~~~~~~~~~~~~~~~~~~~ START Job ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+cd "$MYTMP"
+
+REF_FASTA=cDBG_SRR11015356_k75.unitigs.fa
+
+# Copy required files from the original directory to the scratch directory
+cp /home/mhussien/omnigraph/data/cDBG75/$REF_FASTA ./
+
+
+# Set Global Variables
+SCRIPTS=/home/mhussien/omnigraph/scripts
+THREADS=32
+OUT_PREFIX=cDBG75
+MAX_RAM_MB=50000
+MAX_RAM_GB=50
+K_SIZE=75
+
+############################## (1) START CDHIT ####################################
+
+echo "CD-HIT on %985 ..."
+WORD_SIZE=11
+SIM=0.985
+cd-hit-est -i ${REF_FASTA} -n ${WORD_SIZE} -c ${SIM} -o clusters_${SIM}_${OUT_PREFIX} -d 0 -T ${THREADS} -M ${MAX_RAM_MB}
+
+################################ END CDHIT ######################################
+
+
+##################### (2) EXTRACTING Representatives from REF_FASTA ##################
+
+for SIM in 0.985
+do
+    echo "Extracting representatives from clusters at %${SIM}"
+    cat clusters_${SIM}_${OUT_PREFIX}.clstr | grep "\*" | awk -F"[>.]" '{print ">"$2}' | grep -Fwf - -A1 <(seqkit seq -w 0 ${REF_FASTA}) | grep -v "^\-\-" > reps_unitigs_${OUT_PREFIX}_${SIM}.fa
+done
+
+echo "############################# Stats on reps"
+
+for SIM in 0.985
+do
+    REPS_STD_OUT=stats_reps_unitigs_${OUT_PREFIX}_${SIM}.txt
+    python ${SCRIPTS}/unitigs_to_connected_components.py reps_unitigs_${OUT_PREFIX}_${SIM}.fa > ${REPS_STD_OUT}
+    largest_component=$(cat ${REPS_STD_OUT}| grep -w "largest_comp_bp" | cut -f2)
+    python ${SCRIPTS}/unitigs_to_viz_kmersHistograms.py reps_unitigs_${OUT_PREFIX}_${SIM}.fa reps_unitigs_${OUT_PREFIX}_${SIM}.fa.components.csv ${largest_component}
+    rm ${REPS_STD_OUT}
+done
+
+########################## DONE EXTRACTION Representatives ########################
+
+
+############# (3) Constructing cDBG of the representative sequences ##################
+
+echo "#################### (REDUCED) Constructing cDBG of the representative sequences"
+
+for SIM in 0.985
+do
+    echo "Constructing cDBG of representatives at %${SIM}"
+    bcalm -kmer-size ${K_SIZE} -nb-cores ${THREADS} -max-memory ${MAX_RAM_MB} -abundance-min 1 -out cDBG2_${SIM}_${OUT_PREFIX} -in reps_unitigs_${OUT_PREFIX}_${SIM}.fa # &> log_bcalm_cDBG_reps_unitigs_${OUT_PREFIX}_${SIM}
+    echo "Converting reps_unitigs_${OUT_PREFIX}_${SIM}.fa to GFA"
+    python ${SCRIPTS}/unitigsToGFA.py cDBG2_${SIM}_${OUT_PREFIX}.unitigs.fa cDBG2_${SIM}_${OUT_PREFIX}.unitigs.gfa ${K_SIZE} --single-directed
+    echo "Getting cDBG Stats for cDBG2_${SIM}_${OUT_PREFIX}.unitigs.fa"
+    Bandage info cDBG2_${SIM}_${OUT_PREFIX}.unitigs.gfa # &> log_Bandage_cDBG2_${SIM}_${OUT_PREFIX}.gfa.log
+    python ${SCRIPTS}/unitigs_to_connected_components.py cDBG2_${SIM}_${OUT_PREFIX}.unitigs.fa > stdout_connected_components.tmp  # &> log_script_connectedComponents_cDBG2_${SIM}_${OUT_PREFIX}.gfa.log
+    python ${SCRIPTS}/unitigs_edges_histogram.py cDBG2_${SIM}_${OUT_PREFIX}.unitigs.fa cDBG2_${SIM}_${OUT_PREFIX}.unitigs.fa.components.csv
+    python ${SCRIPTS}/unitigs_to_viz_kmersHistograms.py cDBG2_${SIM}_${OUT_PREFIX}.unitigs.fa cDBG2_${SIM}_${OUT_PREFIX}.unitigs.fa.components.csv ${largest_component}
+
+done
+##################### DONE EXTRACTION Representatives ##################
+
+########################### Move scratch to home dir ###################
+
+rm -rf ${REF_FASTA}
+
+cp -r $MYTMP $SLURM_SUBMIT_DIR
+scontrol show job $SLURM_JOB_ID     # Print out final statistics about resource uses before job exits
+
+# Print out values of the current jobs SLURM environment variables
+env | grep SLURM
+
+scontrol show job ${SLURM_JOB_ID}     # Print out final statistics about resource uses before job exits
+
+sstat --format 'JobID,MaxRSS,AveCPU' -P ${SLURM_JOB_ID}.batch
