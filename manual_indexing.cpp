9d12e3169f95fb37a73ad1ed314c93db8f75e0cf
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-04-18 02:34:57 +0200

    manual indexing final test

--- manual_indexing.cpp
+++ manual_indexing.cpp
@@ -8,8 +8,7 @@
 using phmap::flat_hash_map;
 using namespace std;
 
-string gen_compressed_str(string str)
-{
+string gen_compressed_str(string str) {
     int len = str.length();
     string result;
     for (int i = 0; i < len; i++) {
@@ -21,12 +20,9 @@ string gen_compressed_str(string str)
             i++;
         }
         // Print character and its count
-        if (count == 1)
-        {
+        if (count == 1) {
             result.push_back(str[i]);
-        }
-        else
-        {
+        } else {
             result.push_back(str[i]);
             result.append(to_string(count));
         }
@@ -35,7 +31,7 @@ string gen_compressed_str(string str)
     return result;
 }
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
 
 
     string filename = argv[1];
@@ -45,11 +41,11 @@ int main(int argc, char ** argv) {
 
     const size_t kSize = 75;
 
-    hash<string> hasher;
-
     uint64_t original_kmers_no = 0;
 
     kmerDecoder *KD = new Kmers(filename, chunk_size, kSize);
+    KD->setHashingMode(3); // Setting hashing mode to bigKmerHasher
+
     int counter = 0;
     while (!KD->end()) {
         KD->next_chunk();
@@ -58,12 +54,12 @@ int main(int argc, char ** argv) {
         for (const auto &seq : *KD->getKmers()) {
             for (const auto &kmer : seq.second) {
                 original_kmers_no++;
-                map[hasher(kmer.str)] = counter + ++seqCounter;
+                map[kmer.hash] = counter + ++seqCounter;
             }
         }
     }
 
-    uint64_t inserted_kmers = (uint64_t)map.size();
+    auto inserted_kmers = (uint64_t) map.size();
     cout << "Original kmers: " << original_kmers_no << endl;
     cout << "Inserted kmers: " << inserted_kmers << endl;
     cout << "Lost kmers: " << original_kmers_no - inserted_kmers << endl;
