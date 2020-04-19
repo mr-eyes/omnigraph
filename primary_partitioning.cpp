53441c46d5277723b3fb631a2d17ff7aba5606ca
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-04-19 10:02:09 +0200

    replace progress bar with traditional logging

--- primary_partitioning.cpp
+++ primary_partitioning.cpp
@@ -139,7 +139,7 @@ int main(int argc, char **argv) {
         long sec = milli / 1000;
         milli = milli - 1000 * sec;
         cerr << "Done in: ";
-        cout << min << ":" << sec << ":" << milli << endl;
+        cerr << min << ":" << sec << ":" << milli << endl;
     }
 
     cout << endl << endl;
