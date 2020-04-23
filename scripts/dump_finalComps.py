17463bac7b8179cc06a89c7133e85ce0701dda91
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-04-23 06:36:35 +0200

    completely paralellized final comps dumping

--- scripts/dump_finalComps.py
+++ scripts/dump_finalComps.py
@@ -12,7 +12,7 @@ Output:
 
 
 Run:
-python dump_finalComps.py <db_file> <pairsCountFile> <optional: cutoff (default: 1)>
+python dump_finalComps.py <db_file> <pairsCountFile> <no_cores> <optional: cutoff (default: 1)>
 """
 
 import sqlite3
@@ -20,54 +20,7 @@ import sys
 from collections import defaultdict
 import os
 from tqdm import tqdm
-
-
-class FastaWriter:
-
-    def __init__(self, output_dir, number_of_files):
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-        else:
-            raise ValueError("Cannot create output directory")
-
-        self.fastaWriters = dict()
-        for finalComp_id in range(1, number_of_files + 1):
-            file_name = os.path.join(output_dir, str(finalComp_id) + ".fa")
-            self.fastaWriters[finalComp_id] = open(file_name, 'w')
-
-    def write_seq(self, _finalCompID, PE, R_ID, seq):
-        self.fastaWriters[_finalCompID].write(f">{PE}.{R_ID}\t{_finalCompID}\n{seq}\n")
-
-    def close_all(self):
-        for _, fileHandler in self.fastaWriters:
-            fileHandler.close()
-
-
-class SQLiteReader:
-
-    def __init__(self, sqlite_file):
-        try:
-            self.conn = sqlite3.connect(sqlite_file)
-
-        except sqlite3.Error as err:
-            print(err)
-            _err_msg = f"could not connect to {sqlite_file}"
-            raise ValueError(_err_msg)
-
-        self.sqlite_file = sqlite_file
-
-    def get_table_data(self, table_name):
-        curs = self.conn.execute(f'select * from {table_name}')
-        rows = curs.fetchall()
-        return rows
-
-    def get_rows_curs(self, table_name):
-        curs = self.conn.execute(f'select * from {table_name}')
-        return curs
-
-    def get_num_rows(self):
-        curs = self.conn.execute("select seq from sqlite_sequence where name = 'reads'")
-        return curs.fetchall()[0][0]
+import multiprocessing
 
 
 class ConnectedComponents:
@@ -141,15 +94,16 @@ def get_nodes_sizes(components_file_path):
     return node_to_size
 
 
-if len(sys.argv) < 3:
-    raise ValueError("run: python dump_finalComps.py dump_finalComps.py <db_file> <pairsCountFile> <cutoff>")
+if len(sys.argv) < 4:
+    raise ValueError("run: python dump_finalComps.py dump_finalComps.py <db_file> <pairsCountFile> <no_cores> <cutoff>")
 
 cutoff_threshold = 1
 sqlite_db_path = sys.argv[1]
 pairsCountFile = sys.argv[2]
+no_cores = int(sys.argv[3])
 
-if len(sys.argv) == 4:
-    cutoff_threshold = int(sys.argv[3])
+if len(sys.argv) == 5:
+    cutoff_threshold = int(sys.argv[4])
 
 """
 1. Parse the pairsCount to edges
@@ -174,26 +128,36 @@ number_of_final_components = components.number_of_components
 
 print(f"Number of connected comps: {number_of_final_components}")
 
-# Map old original component node to new final component
-originalComp_to_finalComp = dict()
-for finalCompID, originalComps in components.get_components_dict().items():
-    for originalCompID in originalComps:
-        originalComp_to_finalComp[originalCompID] = finalCompID
-
 """
-3. Create fastaWriter instance 
+3. Multithreaded dumping the partitions to fasta files
 """
 
-fastaWriter = FastaWriter(output_dir=os.path.basename(sqlite_db_path), number_of_files=number_of_final_components)
 
-"""
-4. Dump the partitions
-"""
+def perform_writing(params):
+    file_path, _finalCompID, _originalComps = params
+    conn = sqlite3.connect(sqlite_db_path)
+    _originalComps = tuple(_originalComps)
+    read_1_sql = "select * from reads where seq1_original_component in ({seq})".format(seq=','.join(['?'] * len(_originalComps)))
+    read_1_curs = conn.execute(read_1_sql, _originalComps)
 
-dbReader = SQLiteReader(sqlite_db_path)
+    read_2_sql = "select * from reads where seq1_original_component in ({seq})".format(seq=','.join(['?'] * len(_originalComps)))
+    read_2_curs = conn.execute(read_2_sql, _originalComps)
 
-for row in tqdm(dbReader.get_rows_curs("reads"), total=dbReader.get_num_rows()):
-    ID, R1_seq, R2_seq, R1_originalComp, R2_originalComp = row
-    if R1_originalComp in originalComp_to_finalComp and R2_originalComp in originalComp_to_finalComp:
-        fastaWriter.write_seq(_finalCompID=originalComp_to_finalComp[R1_originalComp], PE=1, R_ID=ID, seq=R1_seq)
-        fastaWriter.write_seq(_finalCompID=originalComp_to_finalComp[R2_originalComp], PE=2, R_ID=ID, seq=R2_seq)
+    with open(file_path, 'w') as fastaWriter:
+        for row in read_1_curs:
+            fastaWriter.write(f">{row[0]}.1\t{_finalCompID}\n{row[1]}\n")
+        for row in read_2_curs:
+            fastaWriter.write(f">{row[0]}.2\t{_finalCompID}\n{row[2]}\n")
+
+
+output_dir = os.path.basename(sqlite_db_path).replace(".db", '')
+os.makedirs(output_dir)
+
+all_params = list()
+for finalComp, originalComps in components.get_components_dict().items():
+    file_name = os.path.join(output_dir, f"{finalComp}.fa")
+    all_params.append((file_name, finalComp, originalComps))
+
+with multiprocessing.Pool(no_cores) as pool:
+    for _ in tqdm(pool.imap_unordered(perform_writing, all_params), total=len(all_params)):
+        pass
